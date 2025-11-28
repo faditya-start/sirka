f233f3f722db6687253b3bf217adcc238a8964e8
Daffa Aditya Putra Ellyas <daffaaditya2607@gmail.com>
2025-11-28 18:49:27 +0000

    fix and feat: fix for package and add feat for users

---
+++ src/controllers/userController.js
@@ -0,0 +1,65 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, age, weight, height, goal } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ status: "error", message: "Email sudah terdaftar" });
+    }
+
+    const user = await User.create({ name, email, password, age, weight, height, goal });
+
+    res.status(201).json({
+      status: "success",
+      message: "Registrasi berhasil",
+      data: {
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        status: "success",
+        message: "Login berhasil",
+        data: {
+          _id: user.id,
+          name: user.name,
+          email: user.email,
+          token: generateToken(user._id),
+        },
+      });
+    } else {
+      res.status(401).json({ status: "error", message: "Email atau password salah" });
+    }
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    res.json({ status: "success", data: users });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
